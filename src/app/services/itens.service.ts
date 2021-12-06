0d9b0bf0a1c72a2d6b704001b3c4daa87a290525
felipehenriquee <fh27.95@gmail.com>
2021-12-06 11:02:07 -0300

    modelos atualizados

--- src/app/services/itens.service.ts
+++ src/app/services/itens.service.ts
@@ -16,26 +16,34 @@ export class ItensService {
       1, 
       "SKID DE P-2000 COM PITSTOP", "./assets/tanques/Skid2000Pitstop.jpg", 
       "Instalação composta com 2 Skids de vasos P-2000 com grupo de regulagem mais 1 Skid de Pit-stop padrão Nacional Gás",
-      "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/SkidsP2000comPitstop_Rev05.glb","./assets/qrcode/Skid P-2000Pitstop.png", "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_skids_p2000_com_pitstop_rev04___1637700692046.usdz"
+      "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/2_SkidsP2000_1_Pitstop_Rev01.glb",
+      "./assets/qrcode/Skid P-2000Pitstop.png", 
+      "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_2_skidsp2000_1_pitstop_rev01___1638550137626.usdz"
       ),
       new Itens(
         2, 
         "SKID DE P-2000 COM VAPORIZADOR ELÉTRICO", "./assets/tanques/Skid2000VaporEletrico.jpg", 
         "Instalação composta com 2 Skids de vasos P-2000 com grupo de regulagem mais 1 Skid de vaporizador elétrico (100 a 250kg/h)",
-        "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/SkidsP2000comVaporizadorEletrico_Rev05.glb","./assets/qrcode/Skid P-2000Vaporizadoreletrico.png", "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_skids_p2000_com_vaporizador_eletrico_rev04___1637700745377.usdz"
+        "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/2_SkidsP2000_1_VaporizadorEletrico_Rev01.glb",
+        "./assets/qrcode/Skid P-2000Vaporizadoreletrico.png", 
+        "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_2_skidsp2000_1_vaporizadoreletrico_rev01___1638550171942.usdz"
         ),
       new Itens(
       3, 
       "SKID DE P-4000 COM VAPORIZADOR ELÉTRICO", "./assets/tanques/Skid4000Vporeletrico.jpg", 
       "Instalação composta com 2 Skids de vasos P-4000 com grupo de regulagem mais 1 Skid de vaporizador elétrico (100 a 250kg/h)",
 
-      "P4000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/SkidsP4000comVaporizadorEletrico_Rev05.glb","./assets/qrcode/Skid P-4000Vaporizadoreletrico.png", "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_skids_p4000_com_vaporizador_eletrico_rev04___1637700861006.usdz"
+      "P4000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/2_SkidsP4000_1_VaporizadorEletrico_Rev01.glb",
+      "./assets/qrcode/Skid P-4000Vaporizadoreletrico.png",
+      "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/2_skidsp4000_1_vaporizadoreletrico_rev01___1638550205118.usdz"
       ),
       new Itens(
         4, 
         "SKID DE P-2000", "./assets/gas.png", 
         "Instalação composta com 1 Skid de vasos P-4000 com grupo de regulagem",
-        "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/SkidsP2000_Rev05.glb","./assets/qrcode/Skid P-2000Pitstop.png", "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/gasIphone.usdz"
+        "P2000","https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/SkidsP2000_Rev05.glb",
+        "./assets/qrcode/Skid P-2000Pitstop.png", 
+        "https://simple-xr-dashboard.s3.sa-east-1.amazonaws.com/models-3d/usdz_skidsp2000_rev05___1638550100974.usdz"
         ),
       
       
